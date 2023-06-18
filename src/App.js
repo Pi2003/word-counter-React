51ba8d54975961f7dd431ea447d6f778eb3e9c81
Pinak <pdtendulkar140203@gmail.com>
2023-06-18 16:01:49 +0530

    commit_1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  // the text entered by the user
+  const [text, setText] = useState('');
+
+  // word count
+  const [wordCount, setWordCount] = useState(0);
+
+  // character count
+  const [charCount, setCharCount] = useState(0);
+
+  const changeHandler = (event) => {
+    setText(event.target.value);
+  };
+
+  useEffect(() => {
+    // array of words
+    const words = text.split(' ');
+
+    // update word count
+    let wordCount = 0;
+    words.forEach((word) => {
+      if (word.trim() !== '') {
+        wordCount++;
+      }
+    });
+    setWordCount(wordCount);
+
+    // update char count (including whitespaces)
+    setCharCount(text.length);
+  }, [text]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <div>
+        <textarea
+          value={text}
+          onChange={changeHandler}
+          placeholder='text here'
+        ></textarea>
+
+        <div>
+          <p className='word-count'>Word Count: {wordCount}</p>
+          <p className='char-count'>
+            Character Count (Including Whitespaces): {charCount}
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
